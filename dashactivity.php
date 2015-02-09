a65c0f8942dbf95e38204f22273e4339fec51f16
indesign47 <transfertpcmac@neuf.fr>
2015-02-09 10:53:25 +0100

    Update dashactivity.php

--- dashactivity.php
+++ dashactivity.php
@@ -1,6 +1,6 @@
 <?php
 /*
-* 2007-2014 PrestaShop
+* 2007-2015 PrestaShop
 *
 * NOTICE OF LICENSE
 *
@@ -19,7 +19,7 @@
 * needs please refer to http://www.prestashop.com for more information.
 *
 *  @author PrestaShop SA <contact@prestashop.com>
-*  @copyright  2007-2014 PrestaShop SA
+*  @copyright  2007-2015 PrestaShop SA
 *  @license    http://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
 *  International Registered Trademark & Property of PrestaShop SA
 */
@@ -43,6 +43,7 @@ class Dashactivity extends Module
 
 		parent::__construct();
 		$this->displayName = $this->l('Dashboard Activity');
+		$this->description = $this->l('Adds a block whith an overview of the current activity on your store.');
 		$this->ps_versions_compliancy = array('min' => '1.6', 'max' => _PS_VERSION_);
 	}
 
